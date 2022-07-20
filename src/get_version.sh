9d74da392f3f82e992074f722aa5774f02922a37
qiaofeng1227 <76487013@qq.com>
2022-07-20 14:57:16 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "magento version:" $(cat /bitnami/magento/composer.json |grep version |cut -d'"' -f 4) |sudo tee -a /data/logs/install_version.txt
+sudo echo "magento version:" $(docker exec -i magento cat /bitnami/magento/composer.json |grep \"version\": |cut -d'"' -f 4) |sudo tee -a /data/logs/install_version.txt
